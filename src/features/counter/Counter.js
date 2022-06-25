bc6a796b16e2639b86160f21ef39a730e379c7a6
imber <a1157911285@163.com>
2022-06-25 16:17:09 +0800

    feat:redux-toolkit初始化

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
   incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+  selectCount
+} from './counterSlice'
+import styles from './Counter.module.css'
 
 export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  // 从store拿数据
+  const count = useSelector(selectCount)
+  // 用于派发reducer
+  const dispatch = useDispatch()
+  const [incrementAmount, setIncrementAmount] = useState('2')
 
-  const incrementValue = Number(incrementAmount) || 0;
+  const incrementValue = Number(incrementAmount) || 0
 
   return (
     <div>
@@ -63,5 +65,5 @@ export function Counter() {
         </button>
       </div>
     </div>
-  );
+  )
 }
